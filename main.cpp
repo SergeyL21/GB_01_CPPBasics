0a3b9ea6d5a43ef7b548c5e78b790596823e966a
Sergey Lola <sergey_lola@mail.ru>
2021-02-02 18:22:49 +0300

    Add prepared cmake project files

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ * Created by Qt Creator 4.11.0 @ 01 Feb 2021 21:21:03
+ *
+ * Project: GB_CppBasics
+ * File: main.cpp
+ *
+ * Author: Sergey Lola
+ */
+
+#include <iostream>
+
+int main(int argc, const char ** argv)
+{
+	return 0;
+}
